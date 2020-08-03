6fb0c191433731e36831e74eac404dbfb36dc099
ObeySridhar <sridharan7614@gmail.com>
2020-08-04 00:43:54 +0530

    Seperated timeline code and added form attribute to post button

---
+++ client/src/components/Timeline.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+
+export default class Timeline extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      timelineEmptyFlag: true,
+      likeCount: 0,
+    };
+  }
+
+  likeHandler = () => {
+    let newCount = this.state.likeCount + 1;
+    this.setState({
+      likeCount: newCount,
+    });
+  };
+  render() {
+    const { imgSrc, text } = this.props.postData;
+    return (
+      <React.Fragment>
+        <section class="middle">
+          <div class="container">
+            <hr />
+            <div class="middle-content">
+              <p>Sorry! No posts available :(</p>
+            </div>
+          </div>
+        </section>
+
+        <section class="bottom">
+          <div class="container">
+            <div class="card-container timeline-card-container">
+              <div class="card-top timeline-card-top">
+                <div class="top-left timeline-top-left">
+                  <div class="avatar">
+                    <img src="images/avatar.jpg" alt="Avatar" />
+                  </div>
+                  <div class="avatar-details">
+                    <h4>Angelina John</h4>
+                    <p>Passionate hair stylist</p>
+                  </div>
+                </div>
+                <div class="top-right timeline-top-right">
+                  <p>Just now</p>
+                </div>
+              </div>
+              <div class="card-middle timeline-card-middle">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit,
+                odio? Expedita dignissimos asperiores nesciunt possimus.
+              </div>
+              <div class="card-bottom timeline-card-bottom">
+                <img src="images/preview-image.jpg" alt="" />
+              </div>
+              <div class="like-comment-share">
+                <i class="far fa-heart lcs"></i>
+                <i class="far fa-comment lcs"></i>
+                <i class="fas fa-share lcs"></i>
+              </div>
+            </div>
+          </div>
+        </section>
+      </React.Fragment>
+    );
+  }
+}
--- client/src/components/uploadImage.js
+++ client/src/components/uploadImage.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from "react";
 import axios from "axios";
 import Navbar from "./navbar";
+import Timeline from "./Timeline";
 import "../styles/style.css";
 
 export default class uploadImage extends Component {
@@ -190,7 +191,7 @@ export default class uploadImage extends Component {
                       onChange={this.onInputChange}
                       ref={(ref) => (this.fileInput = ref)}
                     />
-                    <button>
+                    <button type="submit" form="post-form">
                       {!this.state.isLoading ? (
                         <i className="fas fa-paper-plane load"></i>
                       ) : (
@@ -229,48 +230,11 @@ export default class uploadImage extends Component {
             </div>
           </section>
 
-          <section class="middle">
-            <div class="container">
-              <hr />
-              <div class="middle-content">
-                <p>Sorry! No posts available :(</p>
-              </div>
-            </div>
-          </section>
-
-          <section class="bottom">
-            <div class="container">
-              <div class="card-container timeline-card-container">
-                <div class="card-top timeline-card-top">
-                  <div class="top-left timeline-top-left">
-                    <div class="avatar">
-                      <img src="images/avatar.jpg" alt="Avatar" />
-                    </div>
-                    <div class="avatar-details">
-                      <h4>Angelina John</h4>
-                      <p>Passionate hair stylist</p>
-                    </div>
-                  </div>
-                  <div class="top-right timeline-top-right">
-                    <p>Just now</p>
-                  </div>
-                </div>
-                <div class="card-middle timeline-card-middle">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Fugit, odio? Expedita dignissimos asperiores nesciunt
-                  possimus.
-                </div>
-                <div class="card-bottom timeline-card-bottom">
-                  <img src="images/preview-image.jpg" alt="" />
-                </div>
-                <div class="like-comment-share">
-                  <i class="far fa-heart lcs"></i>
-                  <i class="far fa-comment lcs"></i>
-                  <i class="fas fa-share lcs"></i>
-                </div>
-              </div>
-            </div>
-          </section>
+          {/* TIMELINE */}
+          <Timeline
+            postData={this.state.postData}
+            timelineEmptyFlag={this.state.timelineEmptyFlag}
+          />
         </main>
       </Fragment>
     );
